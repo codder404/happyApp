3f4289046039c2078810dd4aaf8d8cc17b4bd43b
Fernando dos Santos <nando4803@gmail.com>
2020-10-14 23:36:41 +0100

    [web] this commit add two new routes

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,13 +3,16 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
-
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
 const Routes = () => {
   return (
     <Router>
       <Switch>
         <Route path="/" component={Landing} exact />
-        <Route path="/app" component={OrphanagesMap} />
+        <Route path="/app" component={OrphanagesMap} exact />
+        <Route path="/orphanage/create" component={CreateOrphanage} exact />
+        <Route path="/orphanage/:id" component={Orphanage} />
       </Switch>
     </Router>
   )
